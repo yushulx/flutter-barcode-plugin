7d9d149072e87b880bb8730ebf4b12c9c4c8ad36
yushulx <lingxiao1002@gmail.com>
2019-08-11 23:14:39 +0800

    Added initLicense and decodeFile methods, but not implemented yet

--- barcode_reader/ios/Classes/BarcodeReaderPlugin.m
+++ barcode_reader/ios/Classes/BarcodeReaderPlugin.m
@@ -8,12 +8,21 @@
             binaryMessenger:[registrar messenger]];
   BarcodeReaderPlugin* instance = [[BarcodeReaderPlugin alloc] init];
   [registrar addMethodCallDelegate:instance channel:channel];
+
+  DynamsoftBarcodeReader *m_barcodeReader = [[DynamsoftBarcodeReader alloc] init];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
+  }
+  else if ([@"initLicense" isEqualToString:call.method]) {
+    result(@"initLicense");
+  } 
+  else if ([@"decodeFile" isEqualToString:call.method]) {
+    result(@"decodeFile");
+  }
+  else {
     result(FlutterMethodNotImplemented);
   }
 }
